731ab4d930a39d5f3029948de01eabacc2d788ce
Cristian <cristianmihai193@gmail.com>
2021-02-03 19:00:41 +0100

    yield home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,78 +1,46 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('layouts.main')
 
-        <title>LaravelMolisana</title>
-
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@200;600&display=swap" rel="stylesheet">
-        <link rel="stylesheet" href="{{ asset("css/app.css") }}">
-        
-    </head>
-    <body>
-
-      {{-- header --}}
-      <header>
-        <img src="{{ asset('img/marchio-sito-test.png') }}" alt="La molisana">
-        <ul class="list-inline">
-          <li class="active"><a href="">HOME</a></li>
-          <li><a href="">PRODOTTI</a></li>
-          <li><a href="">NEWS</a></li>
-        </ul>
-      </header>
-      {{-- //header --}}
-
-      {{-- main --}}
-      <main>
-        <section class="contenitore">
-          <h2>LE LUNGHE</h2>
-          <div class="cards">
-            @foreach ($lunghe as $pasta)
-            <div class="card">
-              <img src="{{ $pasta['src'] }}" alt="{{ $pasta['titolo'] }}">
-              <div class="card-text">
-                <a href="#">{{ $pasta['titolo'] }}</a>
-              </div>
-            </div>
-            @endforeach
-          </div>
-        </section>
-        <section class="contenitore">
-          <h2>LE CORTE</h2>
-          <div class="cards">
-            @foreach ($corte as $pasta)
-            <div class="card">
-              <img src="{{ $pasta['src'] }}" alt="{{ $pasta['titolo'] }}">
-              <div class="card-text">
-                <a href="#">{{ $pasta['titolo'] }}</a>
-              </div>
-            </div>
-            @endforeach
-          </div>
-        </section>
-        <section class="contenitore">
-          <h2>LE CORTISSIME</h2>
-          <div class="cards">
-            @foreach ($cortissime as $pasta)
-            <div class="card">
-              <img src="{{ $pasta['src'] }}" alt="{{ $pasta['titolo'] }}">
-              <div class="card-text">
-                <a href="#">{{ $pasta['titolo'] }}</a>
-              </div>
-            </div>
-            @endforeach
-          </div>
-        </section>
-        
-      </main>
-      {{-- //main --}}
-
-      {{-- footer --}}
-      <footer>
-
-      </footer>
-      {{-- //footer --}}
-    </body>
-</html>
+@section('content')
+<main>
+  <section class="contenitore">
+    <h2>LE LUNGHE</h2>
+    <div class="cards">
+      @foreach ($lunghe as $pasta)
+      <div class="card">
+        <img src="{{ $pasta['src'] }}" alt="{{ $pasta['titolo'] }}">
+        <div class="card-text">
+          <a href="#">{{ $pasta['titolo'] }}</a>
+        </div>
+      </div>
+      @endforeach
+    </div>
+  </section>
+  <section class="contenitore">
+    <h2>LE CORTE</h2>
+    <div class="cards">
+      @foreach ($corte as $pasta)
+      <div class="card">
+        <img src="{{ $pasta['src'] }}" alt="{{ $pasta['titolo'] }}">
+        <div class="card-text">
+          <a href="#">{{ $pasta['titolo'] }}</a>
+        </div>
+      </div>
+      @endforeach
+    </div>
+  </section>
+  <section class="contenitore">
+    <h2>LE CORTISSIME</h2>
+    <div class="cards">
+      @foreach ($cortissime as $pasta)
+      <div class="card">
+        <img src="{{ $pasta['src'] }}" alt="{{ $pasta['titolo'] }}">
+        <div class="card-text">
+          <a href="#">{{ $pasta['titolo'] }}</a>
+        </div>
+      </div>
+      @endforeach
+    </div>
+  </section>
+  
+</main>
+@endsection
---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,8 @@
+<header>
+  <img src="{{ asset('img/marchio-sito-test.png') }}" alt="La molisana">
+  <ul class="list-inline">
+    <li class="active"><a href="">HOME</a></li>
+    <li><a href="">PRODOTTI</a></li>
+    <li><a href="">NEWS</a></li>
+  </ul>
+</header>
